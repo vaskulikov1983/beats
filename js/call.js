3162223eb9b568c9507a936cb89afedfb8aca5cf
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-09-14 18:02:16 +0500

    changes for nikolay

---
+++ js/call.js
@@ -0,0 +1,12 @@
+let checkboxLabel = document.querySelector('.form__label_checkbox');
+let checkboxInput = checkboxLabel.querySelector('input');
+
+checkboxLabel.addEventListener('click', function () {
+    if (checkboxInput.checked == true) {
+        checkboxInput.checked = false;
+        checkboxLabel.classList.remove('form__label_checkbox_active');
+    } else { 
+        checkboxInput.checked = true;
+        checkboxLabel.classList.add('form__label_checkbox_active');
+    }
+})
---
+++ js/payment.js
@@ -0,0 +1,26 @@
+let paymentType = document.querySelector('.form__payment');
+let cashBlock = paymentType.querySelector('.form__cash');
+let cardBlock = paymentType.querySelector('.form__card');
+let cashLabel = cashBlock.querySelector('label');
+let cardLabel = cardBlock.querySelector('label');
+let cashRadio = cashBlock.querySelector('input');
+let cardRadio = cardBlock.querySelector('input');
+
+paymentType.addEventListener('click', function (evt) { 
+    if (evt.target == cashLabel) { 
+        if (cashRadio.checked == false) { 
+            cashLabel.classList.add('form__label_radio_active');
+            cashRadio.checked == true;
+            cardLabel.classList.remove('form__label_radio_active');
+            cardRadio.checked == false;
+        }
+    }
+    if (evt.target == cardLabel) { 
+        if (cardRadio.checked == false) { 
+            cardLabel.classList.add('form__label_radio_active');
+            cardRadio.checked == true;
+            cashLabel.classList.remove('form__label_radio_active');
+            cashRadio.checked == false;
+        }
+    }
+})
