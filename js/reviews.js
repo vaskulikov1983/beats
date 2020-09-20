68a56d8c8538d027a87ee71df522ce4db800fa3a
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-09-21 02:31:25 +0500

    reviews script

--- js/reviews.js
+++ js/reviews.js
@@ -1,3 +1,18 @@
 $(document).ready(() => { 
-    
+    const reviewsItems = $('.reviews__item');
+    const navButtons = $('.nav-sliders__button');
+
+    $('.nav-sliders__list').on('click', (evt) => { 
+        if ($(evt.target).hasClass('nav-sliders__img')) { 
+            const navIndex = $(evt.target).closest('li').index();
+            reviewsItems.each((ndx, item) => {
+                $(item).removeClass('reviews__item_active');
+            });
+            navButtons.each((ndx, item) => {
+                $(item).removeClass('nav-sliders__button_active');
+            });
+            reviewsItems.eq(navIndex).addClass('reviews__item_active');
+            navButtons.eq(navIndex).addClass('nav-sliders__button_active');
+        }
+    })
 })
