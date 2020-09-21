dddccbd129289baf403e7b2dba203c6af427be19
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-09-21 21:25:56 +0500

    reviwes slider

--- js/reviews.js
+++ js/reviews.js
@@ -3,16 +3,14 @@ $(document).ready(() => {
     const navButtons = $('.nav-sliders__button');
 
     $('.nav-sliders__list').on('click', (evt) => { 
-        if ($(evt.target).hasClass('nav-sliders__img')) { 
+        if ($(evt.target).hasClass('nav-sliders__img')) {
             const navIndex = $(evt.target).closest('li').index();
-            reviewsItems.each((ndx, item) => {
-                $(item).removeClass('reviews__item_active');
-            });
-            navButtons.each((ndx, item) => {
-                $(item).removeClass('nav-sliders__button_active');
-            });
-            reviewsItems.eq(navIndex).addClass('reviews__item_active');
-            navButtons.eq(navIndex).addClass('nav-sliders__button_active');
+
+            $('.reviews__item').eq(navIndex).addClass('reviews__item_active').siblings().removeClass('reviews__item_active');
+            $('.nav-sliders__list .nav-sliders__button').removeClass('nav-sliders__button_active');
+
+            $(evt.target).closest('.nav-sliders__button').addClass('nav-sliders__button_active');
+            $('.reviews__item').eq(navIndex).fadeIn().siblings().fadeOut();
         }
     })
 })
