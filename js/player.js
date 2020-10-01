e0e6ed2af5681f9e0cb249e36173f5774eb59a69
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-10-01 21:57:25 +0500

    side menu and video

--- js/player.js
+++ js/player.js
@@ -16,16 +16,38 @@ let eventsInit = () => {
     })
 }
 
+const formatTime = timeSec => { 
+    const roundTime = Math.round(timeSec);
+    const minutes = addZero(Math.floor(roundTime / 60));
+    const seconds = addZero(roundTime % 60);
+
+    function addZero(num) { 
+        return num < 10 ? `0${num}` : num;
+    }
+    return `${minutes} : ${seconds}`;
+}
+
 const onPlayerReady = () => { 
+    let interval;
+
     const durationSec = player.getDuration();
-    $('.player__duration-estimate').text(durationSec);
+    $('.player__duration-estimate').text(formatTime(durationSec));
+
+    if (typeof interval !== 'undefined') { 
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+        const completedSec = player.getCurrentTime();
+        $('.player__duration-completed').text(formatTime(completedSec));
+    }, 1000);
 }
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('yt-player', {
         height: '392',
         width: '662',
-        videoId: 'QMeew_hKLBU',
+        videoId: 'uCNWuSbPnt4',
         events: {
             controls: 0,
             disablekb: 0,
